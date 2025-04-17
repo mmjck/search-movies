57f5c5ec2b9928a18042bfa812ab1680a1379124
jackson <msantosjck@gmail.com>
2025-04-17 10:14:11 -0400

    fix methods

--- app/src/main/java/com/mmjck/searchmovies/retrofit/MoviesApi.kt
+++ app/src/main/java/com/mmjck/searchmovies/retrofit/MoviesApi.kt
@@ -1,6 +1,5 @@
 package com.mmjck.searchmovies.retrofit
 
-import android.media.Rating
 import com.mmjck.searchmovies.model.Movie
 import com.mmjck.searchmovies.model.MoviesResponse
 import retrofit2.Call
@@ -9,11 +8,15 @@ import retrofit2.http.Query
 
 interface MoviesApi {
 
-    @GET
-    fun getInfoAdditional(@Query("i") rating: String, @Query("apikey") apikey: String = "9b9bce0a") : Call<Movie>
+    @GET(".")
+    fun getInfoAdditional(
+        @Query("i") imdbId: String,
+        @Query("apikey") apikey: String = "b3e9d173"
+    ): Call<Movie>
 
-    @GET("?")
-    fun getMovies(@Query("s") title: String, @Query("apikey") apikey: String = "9b9bce0a") : Call<MoviesResponse>
-
-
+    @GET(".")
+    fun getMovies(
+        @Query("s") title: String,
+        @Query("apikey") apikey: String = "b3e9d173"
+    ): Call<MoviesResponse>
 }
